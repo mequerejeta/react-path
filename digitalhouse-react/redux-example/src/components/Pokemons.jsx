7978dec96b70291c9206a200aac3e321dd40365b
Matias <matiasezequielquerejeta@gmail.com>
2024-07-10 23:11:29 -0300

    Add changes to redux-example course

--- digitalhouse-react/redux-example/src/components/Pokemons.jsx
+++ digitalhouse-react/redux-example/src/components/Pokemons.jsx
@@ -1,5 +1,25 @@
-// import { useSelector } from 'react-redux'
-// export const  Pokemons () => {
+import { useDispatch, useSelector } from 'react-redux'
+export const  Pokemons = () => {
 
-//     const {} = useSelector(state => state.pokemons)
-// }
+    const {isLoading, pokemons = [], page, error } = useSelector(state => state.pokemons)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        
+       dispatch(getPokemons())
+    }, []);
+    return(
+        <div>
+            <h1>Pokemons</h1>
+            {error && <p>Algo a salido mal</p>}
+            {isLoading && <p>Cargando ....</p> }
+            <div>
+                {pokemons.map((pokemon) => (
+                   <p key={pokemon.name}>{pokemon.name}</p> 
+                ))}
+            </div>
+            <button onClick = {() => dispatch(getPokemons(page))}>
+                Next Page
+            </button>
+        </div>
+    )
+}
